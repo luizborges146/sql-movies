99994f33233e297fa38c62f3bd52d28e104a46ab
luizborges146 <113717656+luizborges146@users.noreply.github.com>
2022-10-31 14:04:34 -0700

    Tested the delete function

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'Zjwin19970517@',
+  password: 'Test1234!',
   database: 'movie_db'
 });
 
@@ -26,7 +26,7 @@ app.get('/api/movies', (req, res) => {
 });
 
 app.delete('/api/movies/:id', (req, res) => {
-  connection.query(`DELETE id FROM movies`, (err, result) => {
+  connection.query("DELETE FROM movies WHERE id = ?", [response.move_id], (err, result) => { 
     if (err) {
       res.status(500).json({ error: err.message });
       return;
@@ -38,3 +38,26 @@ app.delete('/api/movies/:id', (req, res) => {
 app.listen(PORT, () => {
   console.log(`This is the port localhost${PORT}`)
 })
+
+
+
+// +----+------------+
+// | id | movie_name |
+// +----+------------+
+// |  1 | Matrix     |
+// |  2 | Spider-Man |
+// |  3 | Rocky      |
+// |  4 | Holloween  |
+// |  5 | Top gun    |
+// +----+------------+
+
+// +----+---------+----------+
+// | id | review  | movie_id |
+// +----+---------+----------+
+// |  1 | 4 Stars |        1 |
+// |  2 | 3 Stars |        2 |
+// |  3 | 4 Stars |        3 |
+// |  4 | 3 Stars |        4 |
+// |  5 | 5 Stars |        5 |
+// |  6 | 2 Stars |        2 |
+// +----+---------+----------+
